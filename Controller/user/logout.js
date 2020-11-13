d1f83767646d3f0f2f42c81fb684862c1b066020
ZAELROOT <50934329+ZAELROOT@users.noreply.github.com>
2020-11-13 16:13:04 +0900

    Add feature at User Controller #12 Add feature logout Add feature signup

--- Controller/user/logout.js
+++ Controller/user/logout.js
@@ -1,7 +1,17 @@
-const {users} = require('../../models/users');
-
 module.exports = {
     post: (req,res) =>{
-        
+        const sess = req.session;
+
+        if(sess.userid){
+            req.session.destroy(err =>{
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/');// 메인페이지로 이동 *수정필요
+                }
+            });
+        }else{
+            res.redirect('/');
+        }
     }
 };
