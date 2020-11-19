b8116eb2e712d765b768eb57b688aa01091b1512
ZAELROOT <50934329+ZAELROOT@users.noreply.github.com>
2020-11-19 22:17:44 +0900

    bug fix all #47 bug fix all

--- Controller/post/postpush.js
+++ Controller/post/postpush.js
@@ -1,34 +1,21 @@
 const {post_lists} = require('../../models');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+const YOUR_SECRET_KEY = process.env.SECRET_KEY;
 
 module.exports = {
     post:(req,res) =>{
-        const {title,article,id} = req.body;
-        post_lists.findOrCreate({
-            where:{
-                id: id
-            },
-            defaults: {
-                id: id,
-                title:title,
-                article:article,
-            }
+        let {title,article} = req.body;
+        post_lists
+        .create({
+            title:title,
+            article:article,
+            userId: jwt.verify(req.cookies.userid,YOUR_SECRET_KEY).id
         })
-        .then(async ([post,created])=>{
-            if(!created){
-                post_lists.update({title:title,article:article},{
-                    where: {
-                        id: id
-                    }
-                });
-                res.status(201).send("글 수정이 완료되었습니다.");
-            }else{
-                const data = await post.get({plain:true});
-                res.status(201).send(data);
-            }
-            
+        .then(result=>{
+            res.status(201).send(result);
         })
-        .catch(err=>{
-            res.status(500).send(err);
-        })
+        .catch(err=>{res.status(500).send(err)});
     }
 };
